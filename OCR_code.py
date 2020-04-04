3c88268c05809769d92fdc6cfaa38e58c87db00f
RiadAhmed-prog <53094420+RiadAhmed-prog@users.noreply.github.com>
2020-04-04 13:38:58 +0600

    Updated

--- OCR_code.py
+++ OCR_code.py
@@ -8,7 +8,7 @@ Created on Wed Apr  1 22:19:23 2020
 
 import cv2
 import pytesseract
-img_cv = cv2.imread('croped napa.jpg')
+img_cv = cv2.imread('new.jpg')
 
 alpha = 1.5 # Contrast control (1.0-3.0)
 beta = 0 # Brightness control (0-100)
@@ -16,7 +16,6 @@ beta = 0 # Brightness control (0-100)
 # we need to convert from BGR to RGB format/mode:
 adjusted = cv2.convertScaleAbs(img_cv, alpha=alpha, beta=beta)
 gray = cv2.cvtColor(adjusted, cv2.COLOR_BGR2GRAY)
-img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
 print(pytesseract.image_to_string(gray))
 # OR
 '''img_rgb = Image.frombytes('RGB', img_cv.shape[:2], img_cv, 'raw', 'BGR', 0, 0)
